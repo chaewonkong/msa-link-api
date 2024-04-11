5ed337812737390a65a4bf44975d5b024604fd8e
chaewonkong <chaewonkong@gmail.com>
2024-04-11 20:37:10 +0900

    feat: implement update repo

--- link/payload.go
+++ link/payload.go
@@ -7,6 +7,14 @@ type AddPayload struct {
 	Description string `json:"description"`
 }
 
+// UpdatePayload represents the payload to update a link
+type UpdatePayload struct {
+	ID             uint   `json:"id"`
+	Title          string `json:"title"`
+	Description    string `json:"description"`
+	ThumbnailImage string `json:"thumbnail_image"`
+}
+
 // QueuePayload represents the payload to send to the queue
 type QueuePayload struct {
 	ID  uint   `json:"id"`
--- link/repository.go
+++ link/repository.go
@@ -13,11 +13,33 @@ func NewRepository(db *gorm.DB) *Repository {
 }
 
 // Add adds a new link
-func (r *Repository) Add(l AddPayload) (res Link, err error) {
+func (r *Repository) Add(p AddPayload) (res Link, err error) {
 	// save link to database
 
-	link := Link{URL: l.URL, Title: l.Title, Description: l.Description}
+	link := Link{URL: p.URL, Title: p.Title, Description: p.Description}
 	tx := r.db.Create(&link)
+
+	if tx.Error != nil {
+		err = tx.Error
+		return
+	}
+
+	if tx.RowsAffected == 0 {
+		err = gorm.ErrRecordNotFound
+		return
+	}
+
+	res = link
+	return
+}
+
+// Update updates a link
+func (r *Repository) Update(p UpdatePayload) (res Link, err error) {
+	link := Link{
+		Model: gorm.Model{ID: p.ID},
+	}
+	tx := r.db.Model(&link).
+		Updates(Link{Title: p.Title, Description: p.Description, ThumbnailImage: p.ThumbnailImage})
 
 	if tx.Error != nil {
 		err = tx.Error
